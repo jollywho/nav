a2dbd82fbcdb1f49108e4e162091aa003be197aa
Kevin Vollmer <works.kvollmer@gmail.com>
2015-11-16 00:56:03 -0500

    vert separator

--- src/fnav/tui/overlay.c
+++ src/fnav/tui/overlay.c
@@ -13,6 +13,10 @@ Overlay* overlay_new()
 
 void overlay_clear(Overlay *ov)
 {
+  if (ov->separator) {
+    wclear(ov->nc_win_sep);
+    wnoutrefresh(ov->nc_win_sep);
+  }
   wclear(ov->nc_win_st);
   wnoutrefresh(ov->nc_win_st);
 }
@@ -23,27 +27,50 @@ void overlay_set(Overlay *ov, Buffer *buf)
   pos_T size = buf_size(buf);
   pos_T ofs  = buf_ofs(buf);
   size.lnum--;
-  buf_set_size(buf, size);
+  if (ofs.col == 0) {
+    ov->separator = 0;
+  }
+  else {
+    ov->separator = 1;
+    ofs.col++;
+    size.col--;
+  }
+
   overlay_clear(ov);
+  buf_set_size(buf, size);
+  buf_set_ofs(buf, ofs);
   ov->ov_size = size;
   ov->ov_ofs = (pos_T){ ofs.lnum + size.lnum , ofs.col };
+
   wresize(ov->nc_win_st, 1, ov->ov_size.col);
   mvwin(ov->nc_win_st, ov->ov_ofs.lnum, ov->ov_ofs.col);
-  //mvwin(ov->nc_win_sep, ofs.lnum, ofs.col);
-  //wresize(ov->nc_win_sep, size.lnum, 1);
+
+  wresize(ov->nc_win_sep, size.lnum + 1, 1);
+  mvwin(ov->nc_win_sep, ofs.lnum, ofs.col - 1);
+
   window_req_draw(ov, overlay_draw);
 }
 
 void overlay_draw(void **argv)
 {
   Overlay *ov = argv[0];
+
   wattron(ov->nc_win_st, COLOR_PAIR(4));
   mvwaddstr(ov->nc_win_st, 0, 0, "   FM    ");
   wattroff(ov->nc_win_st, COLOR_PAIR(4));
+
   wattron(ov->nc_win_st, COLOR_PAIR(3));
+  wattron(ov->nc_win_sep, COLOR_PAIR(3));
+
   mvwhline (ov->nc_win_st, 0, 9, ' ', ov->ov_size.col);
-  //mvwvline (ov->nc_win_sep, 0, ov->ov_size.col-1, ACS_VLINE, 1);
-  //mvwaddstr(ov->nc_win_st, 0, 0, "     ");
+
+  if (ov->separator)
+    mvwvline (ov->nc_win_sep, 0, 0, ' ', ov->ov_size.lnum + 1);
+
   wattroff(ov->nc_win_st, COLOR_PAIR(3));
+  wattroff(ov->nc_win_sep, COLOR_PAIR(3));
+
   wnoutrefresh(ov->nc_win_st);
+  if (ov->separator)
+    wnoutrefresh(ov->nc_win_sep);
 }
