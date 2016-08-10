jollywho/nav

commit b41fbe83225572f815c52532b3f53a71c0d34f0a

op: repeat last exec with opgroup support

src/nav/event/shell.c
@@ -5,6 +5,7 @@
 #include "nav/log.h"
 #include "nav/option.h"
 #include "nav/plugins/out/out.h"
+#include "nav/plugins/op/op.h"
 
 static void out_data_cb(Stream *, RBuffer *, size_t, void *,  bool);
 static void shell_write_cb(Stream *, void *, int);
@@ -86,6 +87,8 @@ void shell_start(Shell *sh)
       process_exit(&sh->uvproc.process, 0, 0);
     return;
   }
+
+  op_set_exec_line(proc->argv[2], NULL);
   sh->blocking = true;
   wstream_init(proc->in, 0);
   rstream_init(proc->out, 0);

src/nav/cmd.c
@@ -880,10 +880,14 @@ void exec_line(Cmdstr *cmd, char *line)
 {
   log_msg("CMD", "exec_line");
   char *str = strstr(line, "!");
-  ++str;
-  char *pidstr;
-  int pid = shell_exec(str, fs_pwd());
 
+  int pid = -1;
+  if (++str && *str == '!')          // :!!
+    pid = op_repeat_last_exec();
+  else                               // :!
+    pid = shell_exec(str, fs_pwd());
+
+  char *pidstr;
   asprintf(&pidstr, "%d", pid);
   ret2caller(cmd, (Cmdret){STRING, .val.v_str = pidstr});
   free(pidstr);

src/nav/cmdline.c
@@ -319,19 +319,16 @@ static void cmdline_tokenize(Cmdline *cmdline)
   cmdline->cont = esc;
 }
 
-static void check_flags(Cmdline *cmdline, Cmdstr *cmd, Token *word)
+static void check_flags(Cmdline *cmdline, Cmdstr *cmd)
 {
-  /* first token or first token in cmdstr */
-  if (word)
-    word = (Token*)utarray_next(cmdline->tokens, word);
-  else
-    word = (Token*)utarray_front(cmdline->tokens);
-  if (!word)
+  Token *fst = (Token*)utarray_front(cmdline->tokens);
+  char *exstr = token_val(fst, VAR_STRING);
+
+  if (!exstr || cmd_conditional())
     return;
 
-  char *str = token_val(word, VAR_STRING);
-  if (str[0] == '!' && !cmd_conditional())
-    cmd->exec = 1;
+  if (exstr[0] == '!')
+    cmd->exec = true;
 }
 
 Token* cmdline_tokbtwn(Cmdline *cmdline, int st, int ed)
@@ -491,6 +488,16 @@ static bool valid_arry(Cmdline *cmdline, QUEUE *stack, Token *headref)
   return valid;
 }
 
+static bool valid_exec(Cmdline *cmdline, Cmdstr *cmd, Token *token)
+{
+  if (utarray_eltidx(cmdline->tokens, token) != 1)
+    return false;
+
+  Token *prev = (Token*)utarray_prev(cmdline->tokens, token);
+  cmd->rev = prev->end == token->start;
+  return cmd->rev;
+}
+
 static Token* cmdline_parse(Cmdline *cmdline, Token *word, UT_array *parent)
 {
   char ch;
@@ -507,7 +514,7 @@ static Token* cmdline_parse(Cmdline *cmdline, Token *word, UT_array *parent)
   Token *headref = stack_head(stack);
   utarray_new(cmd.chlds, &chld_icd);
 
-  check_flags(cmdline, &cmd, word);
+  check_flags(cmdline, &cmd);
 
   int idx = 0;
   while ((word = (Token*)utarray_next(cmdline->tokens, word))) {
@@ -556,10 +563,8 @@ static Token* cmdline_parse(Cmdline *cmdline, Token *word, UT_array *parent)
       case ',':
         break;
       case '!':
-        if (utarray_eltidx(cmdline->tokens, word) == 1) {
-          cmd.rev = 1;
+        if (valid_exec(cmdline, &cmd, word))
           break;
-        }
       /*FALLTHROUGH*/
       default:
         seek = seek_ahead(cmdline, stack, word);

src/nav/cmdline.h
@@ -24,10 +24,10 @@ typedef struct {
 #define VAR_LIST    4       /* "v_list" is used            */
 
 struct Token {
-  int start;                /* start pos of token in line */
-  int end;                  /* start pos of token in line */
   bool quoted;
   typ_T var;
+  int start;                /* start pos of token in line */
+  int end;                  /* start pos of token in line */
 };
 
 struct Pair {
@@ -49,12 +49,12 @@ struct Cmdret {
 };
 
 struct Cmdstr {
+  QUEUE stack;
+  Cmdret ret;          //return value
+  Token args;
   bool bar;            /* '|' cmd separator */
   bool rev;            /* reverse flag */
-  QUEUE stack;
-  Token args;
-  int exec;            //exec flag
-  Cmdret ret;          //return value
+  bool exec;           //exec flag
   int st;              //start pos of cmdstr
   int ed;              //end   pos of cmdstr
   int idx;             //index in cmdline
@@ -63,16 +63,16 @@ struct Cmdstr {
 };
 
 struct Cmdline {
+  QUEUE refs;          //queue of token refs to avoid recursive cleanup
+  bool cont;           //line continuation
+  bool err;
+  int len;
+  int lvl;             //subexpression level
   UT_array *cmds;      //list of cmdstr
   UT_array *tokens;    //list of tokens
   UT_array *vars;      //list of vars
   UT_array *arys;      //list of arys
-  QUEUE refs;          //queue of token refs to avoid recursive cleanup
   char *line;          //the raw string being built upon
-  int len;
-  int lvl;             //subexpression level
-  bool cont;           //line continuation
-  bool err;
 };
 
 void cmdline_build(Cmdline *cmdline, char *line);

src/nav/expand.c
@@ -110,7 +110,7 @@ static char* human_time(time_t const *t)
   if (!tm)
     return strdup("*** invalid date/time ***");
 
-  strftime(str, sizeof str, "%Y-%m-%d %H:%M:%S", tm);
+  strftime(str, sizeof(str), "%Y-%m-%d %H:%M:%S", tm);
   return strdup(str);
 }
 
@@ -122,10 +122,12 @@ static varg_T stat_type(const char *name)
   if (!buf_attached(buf))
     return arg;
 
+  struct stat *sb = model_curs_value(buf->hndl->model, "stat");
+  if (!sb)
+    return arg;
+
   arg.argc = 1;
   arg.argv = malloc(sizeof(char*));
-
-  struct stat *sb = model_curs_value(buf->hndl->model, "stat");
 
   if (!strcmp("mtime", name))
     arg.argv[0] = human_time(&sb->st_mtim.tv_sec);
@@ -138,7 +140,7 @@ static varg_T stat_type(const char *name)
   else if (!strcmp("inode", name))
     asprintf(&arg.argv[0], "%ld", sb->st_ino);
   else if (!strcmp("device", name))
-    asprintf(&arg.argv[0], "%ld", sb->st_ino);
+    asprintf(&arg.argv[0], "%ld", sb->st_dev);
   else if (!strcmp("nlink", name))
     asprintf(&arg.argv[0], "%ld", sb->st_nlink);
   else if (!strcmp("size", name))
@@ -217,6 +219,9 @@ char* yank_symbol(const char *key, const char *alt)
 char* expand_symbol(const char *key, const char *alt)
 {
   log_msg("EXPAND", "expand symb {%s:%s}", key, alt);
+  //TODO: use %% over %:%
+  //if key[0] == '%' && !key[1]
+  //  get_type()
 
   varg_T ret = get_type(key, alt);
   if (!ret.argc)

src/nav/option.c
@@ -246,11 +246,11 @@ char* opt_var(Token *word, nv_block *blk)
     }
   }
 
-  if (!key || !key[0])
+  if (!key)
     return strdup("");
-  if (*key == '%')
+  else if (*key == '%')
     return expand_symbol(key+1, alt);
-  if (*key == '$')
+  else if (*key == '$')
     key++;
 
   nv_var *var = NULL;

src/nav/plugins/op/op.c
@@ -7,6 +7,7 @@
 #include "nav/option.h"
 #include "nav/event/hook.h"
 #include "nav/event/event.h"
+#include "nav/event/shell.h"
 #include "nav/event/fs.h"
 #include "nav/cmdline.h"
 #include "nav/cmd.h"
@@ -26,6 +27,12 @@ typedef struct {
 void pid_list()
 {
   record_list("op_procs", "pid", "group");
+}
+
+void op_set_exec_line(char *line, void *grp)
+{
+  SWAP_ALLOC_PTR(op.last_line, strdup(line));
+  op.lastgrp = grp;
 }
 
 static void add_pid(char *name, int pid)
@@ -139,6 +146,8 @@ static void create_proc(nv_group *grp, char *line)
     free(proc);
     return;
   }
+
+  op_set_exec_line(line, grp);
   uv_unref((uv_handle_t*)&proc->proc);
   add_pid(grp->key, proc->proc.pid);
 }
@@ -209,6 +218,7 @@ void op_delgrp(Op_group *opgrp)
 {
   if (!opgrp)
     return;
+
   free(opgrp->before);
   free(opgrp->after);
   free(opgrp);
@@ -238,12 +248,23 @@ char* op_status_last()
   return str;
 }
 
+int op_repeat_last_exec()
+{
+  if (op.lastgrp)
+    run_group(op.lastgrp, op.last_line, true);
+  else
+    return shell_exec(op.last_line, fs_pwd());
+
+  return op.last_pid;
+}
+
 void op_new(Plugin *plugin, Buffer *buf, char *arg)
 {
   log_msg("OP", "INIT");
   op.base = plugin;
   op.curgrp = NULL;
   op.last_pid = 0;
+  op.last_line = strdup("");
   plugin->top = &op;
   plugin->name = "op";
   hook_add_intl(-1, plugin, plugin, fileopen_cb,  "fileopen");
@@ -255,7 +276,6 @@ void op_new(Plugin *plugin, Buffer *buf, char *arg)
   if (tbl_mk("op_procs")) {
     tbl_mk_fld("op_procs", "group", TYP_STR);
     tbl_mk_fld("op_procs", "pid",   TYP_STR);
-    //tbl_mk_fld("op_procs", "status", typSTRING);
   }
   uv_signal_start(&mainloop()->children_watcher, chld_handler, SIGCHLD);
 }

src/nav/plugins/op/op.h
@@ -16,9 +16,11 @@ struct Op_group {
 struct Op {
   Plugin *base;
   Handle *hndl;
-  void *curgrp; /* nv_group* */
+  void *curgrp;  /* nv_group* */
+  void *lastgrp; /* nv_group* */
   int last_pid;
   int last_status;
+  char *last_line;
 };
 
 void op_new(Plugin *plugin, Buffer *buf, char *arg);
@@ -30,8 +32,11 @@ void      op_delgrp(Op_group *);
 Cmdret op_kill();
 void pid_list();
 void op_set_exit_status(int);
+void op_set_exec_line(char *, void *);
 char* op_pid_last();
 char* op_status_last();
 void* op_active_group();
+
+int op_repeat_last_exec();
 
 #endif

commit 18762e38a667adf5485428821ac435f1e96ce519

fix img -> shelll broken in b41fbe8

src/nav/event/shell.c
@@ -88,7 +88,9 @@ void shell_start(Shell *sh)
     return;
   }
 
-  op_set_exec_line(proc->argv[2], NULL);
+  if (sh->reg)
+    op_set_exec_line(proc->argv[2], NULL);
+
   sh->blocking = true;
   wstream_init(proc->in, 0);
   rstream_init(proc->out, 0);